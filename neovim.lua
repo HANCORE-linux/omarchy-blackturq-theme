f6d91394ee7d50f62410d9de7873c5be0bbed63e
HANCORE <m.fattah-ahmad@gmx.de>
2026-04-16 00:16:33 +0200

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -1,44 +1,47 @@
 return {
-	{
-		"bjarneo/aether.nvim",
-		name = "aether",
-		priority = 1000,
-		opts = {
-			disable_italics = false,
-			colors = {
-				-- Monotone shades (base00-base07)
-				base00 = "#0a0a0a", -- Default background
-				base01 = "#1A1A1A", -- Lighter background (status bars)
-				base02 = "#322F3B", -- Selection background
-				base03 = "#818893", -- Comments, invisibles
-				base04 = "#8FECD5", -- Dark foreground accent
-				base05 = "#FFF5EE", -- Default foreground
-				base06 = "#D5F2E8", -- Light foreground
-				base07 = "#A9D1D7", -- Light background hint
+  {
+    "bjarneo/aether.nvim",
+    branch = "v2",
+    name = "aether",
+    priority = 1000,
+    opts = {
+      transparent = false,
+      colors = {
+        -- Background colors
+        bg = "#0a0a0a",
+        bg_dark = "#0a0a0a",
+        bg_highlight = "#fc93e4",
 
-				-- Accent colors (base08-base0F)
-				base08 = "#D35F5F", -- Variables, errors, red
-				base09 = "#C89B5D", -- Integers, constants, gold-bronze
-				base0A = "#36BFBF", -- Classes, types, teal-turquoise
-				base0B = "#ADF0E9", -- Strings, bright turquoise-cyan
-				base0C = "#75DDDD", -- Support, regex, cyan-turquoise
-				base0D = "#40E0D0", -- Functions, keywords, main turquoise accent
-				base0E = "#8FECD5", -- Keywords, storage, soft mint
-				base0F = "#485362", -- Deprecated, dark slate accent
-			},
-		},
-		config = function(_, opts)
-			require("aether").setup(opts)
-			vim.cmd.colorscheme("aether")
+        -- Foreground colors
+        fg = "#646d7a",
+        fg_dark = "#485362",
 
-			-- Enable hot reload
-			require("aether.hotreload").setup()
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "aether",
-		},
-	},
+        -- comment: Line highlight, gutter elements, disabled states
+        comment = "#322F3B",
+
+        -- Accent colors
+        red = "#D35F5F",
+        orange = "#F6AE2C",
+        yellow = "#7cafb7",
+        green = "#8FECD5",
+        cyan = "#5a676b",
+        blue = "#ADF0E9",
+        purple = "#9f6cdd",
+        magenta = "#485362",
+      },
+    },
+    config = function(_, opts)
+      require("aether").setup(opts)
+      vim.cmd.colorscheme("aether")
+      -- Enable hot reload
+      require("aether.hotreload").setup()
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "aether",
+    },
+  },
 }
